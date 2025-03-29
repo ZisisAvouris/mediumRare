b4e22b5b94c7bfb69632ff455bb30c300d451028
Zisis Avouris <zisisavouris@gmail.com>
2025-03-29 14:59:18 +0200

    real initial commit

---
+++ mediumRare/include/ImGuiComponents.hpp
@@ -0,0 +1,10 @@
+#include <lvk/HelpersImGui.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace mr {
+	static constexpr float COMPONENT_PADDING = 20.0f;
+
+	ImVec2 ImGuiFPSComponent( const float fps, const ImVec2 pos = { 10, 10 } );
+	ImVec2 ImGuiCameraControlsComponent( glm::vec3 &cameraPos, glm::vec3 &cameraAngles, bool &changedCameraType, const ImVec2 pos = { 10, 10 } );
+}
---
+++ mediumRare/src/ImGuiComponents.cpp
@@ -0,0 +1,47 @@
+#include "../include/ImGuiComponents.hpp"
+
+ImVec2 mr::ImGuiFPSComponent( const float fps, const ImVec2 pos ) {
+	ImGui::SetNextWindowPos( pos );
+	ImGui::Begin( "Stats:", nullptr, ImGuiWindowFlags_AlwaysAutoResize );
+		ImGui::Text("FPS: %i", int(fps) );
+		ImGui::Text("Frametime: %.2f ms", 1000.0f / fps );
+		const ImVec2 componentSize = ImGui::GetItemRectMax();
+	ImGui::End();
+	return componentSize;
+}
+
+ImVec2 mr::ImGuiCameraControlsComponent( glm::vec3 &cameraPos, glm::vec3 &cameraAngles, bool &changedCameraType, const ImVec2 pos ) {
+	static const char *cameraType = "FirstPerson";
+	static const char *comboBoxItems[] = { "FirstPerson", "MoveTo" };
+	static const char *currentComboBoxItem = cameraType;
+
+	ImGui::SetNextWindowPos( pos );
+	ImGui::Begin( "Camera Controls:", nullptr, ImGuiWindowFlags_AlwaysAutoResize );
+		if ( ImGui::BeginCombo( "##combo", currentComboBoxItem ) ) {
+			for ( int n = 0; n < IM_ARRAYSIZE( comboBoxItems ); ++n ) {
+				const bool isSelected = currentComboBoxItem == comboBoxItems[n];
+
+				if ( ImGui::Selectable( comboBoxItems[n], isSelected ) ) {
+					currentComboBoxItem = comboBoxItems[n];
+				}
+				if ( isSelected ) {
+						ImGui::SetItemDefaultFocus();
+				}
+			}
+			ImGui::EndCombo();
+		}
+
+		if ( !strcmp( cameraType, "MoveTo" ) ) {
+			ImGui::SliderFloat3( "Position", glm::value_ptr( cameraPos ), -10.0f, 10.0f );
+			ImGui::SliderFloat3( "Pitch/Pan/Roll", glm::value_ptr( cameraAngles ), -180.0f, 180.0f );
+		}
+
+		if ( currentComboBoxItem && strcmp( currentComboBoxItem, cameraType ) ) {
+			printf( "[INFO] Selected new camera type: %s\n", currentComboBoxItem );
+			cameraType = currentComboBoxItem;
+			changedCameraType = !changedCameraType;
+		}
+		const ImVec2 componentSize = ImGui::GetItemRectMax();
+	ImGui::End();
+	return componentSize;
+}
